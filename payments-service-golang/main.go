67a8448e04a353bd25f3ad48fb68219bc7f2f535
Natã Hobus <natahobus.dev@gmail.com>
2025-09-28 17:05:03 -0300

    melhorar configuração CORS no serviço de pagamentos

--- payments-service-golang/main.go
+++ payments-service-golang/main.go
@@ -48,11 +48,13 @@ func main() {
 	// CORS middleware
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			
 			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
 				return
 			}
 			
