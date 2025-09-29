491510c0c86d49210a34b441de6d5842ad18a54c
Natã Hobus <natahobus.dev@gmail.com>
2025-09-29 15:42:43 -0300

    adicionar middleware de segurança no serviço de pagamentos

--- payments-service-golang/main.go
+++ payments-service-golang/main.go
@@ -58,6 +58,16 @@ func main() {
 		})
 	})
 	
+	// Security middleware
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("X-Frame-Options", "DENY")
+			w.Header().Set("X-XSS-Protection", "1; mode=block")
+			next.ServeHTTP(w, r)
+		})
+	})
+	
 	// CORS middleware
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
