2d7b2b66c8904748dc6aa1cff8407d1737221dca
Natã Hobus <natahobus.dev@gmail.com>
2025-10-01 18:53:47 -0300

    adicionar configuração de ambiente de produção

---
+++ ecommerce-frontend-angular/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.ecommerce.com/api',
+  paymentsUrl: 'https://payments.ecommerce.com/api',
+  wsUrl: 'wss://payments.ecommerce.com/ws'
+};
